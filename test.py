c9e3612fe44733cc72511af4459f2d3ac6a90da9
Konstantin Avramidis <48788671+PonderWOnder@users.noreply.github.com>
2020-11-27 14:32:44 +0100

    i have no idea what's the problem

--- test.py
+++ test.py
@@ -4,9 +4,13 @@ import os
 from OhOne import augmentext
 
 if __name__ == "__main__":
+<<<<<<< Updated upstream
     liste=[os.path.abspath(''),
            'https://medium.com/@justinboylantoomey/fast-text-extraction-with-python-and-tika-41ac34b0fe61',
            'https://en.wikipedia.org/wiki/Tokenization_(lexical_analysis)']
+=======
+    liste=['F:\Desktop\\_AugmenText','https://en.wikipedia.org/wiki/Tokenization']
+>>>>>>> Stashed changes
     sometest=augmentext(liste)
     sometest.run()
 
