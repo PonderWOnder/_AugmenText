a65cf395e45ad43e796a602a9724184564a30cab
ds181026 <ds181026@fhstp.ac.at>
2020-11-30 17:08:19 +0100

    Updated documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,18 +12,14 @@
 #
 import os
 import sys
-
-sys.path.insert(0, os.path.dirname(os.path.abspath('.')))
-
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
 
-
-project = '_Augmentext'
-copyright = '2020, Schmatz, Pasic, Braun, Avramidis'
-author = 'Schmatz, Pasic, Braun, Avramidis'
-
+project = 'Augmentext'
+copyright = '2020, Andreas Braun'
+author = 'Andreas Braun'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1'
@@ -34,9 +30,7 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 
 
 # Add any paths that contain templates here, relative to this directory.
@@ -53,7 +47,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
