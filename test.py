bbc485b15e95603241dbb06636c23c79e1648de5
Konstantin Avramidis <48788671+PonderWOnder@users.noreply.github.com>
2020-12-01 09:31:15 +0100

    test now works with subclass for more abstraction

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import os
-from OhOne import aug_input
+from OhOne import aug_input, aug_loader
 
 if __name__ == "__main__":
     liste=[os.path.abspath(''),
            'https://medium.com/@justinboylantoomey/fast-text-extraction-with-python-and-tika-41ac34b0fe61',
            'https://en.wikipedia.org/wiki/Tokenization_(lexical_analysis)']
-    sometest=aug_input(liste)
+    sometest=aug_input(liste)._run_2()
 
 
