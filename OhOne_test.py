69316745ec5b6b969139eff1e6d1a728d8621445
Konstantin Avramidis <48788671+PonderWOnder@users.noreply.github.com>
2020-12-03 13:25:07 +0100

    added compairing __str__ method

--- OhOne_test.py
+++ OhOne_test.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 13 09:37:08 2020
+
+@author: 'Schmatz, Pasic, Braun, Avramidis'
+E-Mail: 'ds181019,ds182001,ds181026,ds181009@fhstp.ac.at'
+"""
 
 import os
 from sys import stdout
+from functools import reduce
 from OhOne import aug_loader
 from Augmentext_Functions import spell_mistake
         
@@ -18,11 +25,11 @@ class augmentor(spell_mistake,aug_loader):
         self.buffer=[]
             
     def get_fresh_tokens(self,pos=0,word=True):
-        self.token_list=[self.bib[key][word] for key in self.bib][pos]
+        self.start=[self.bib[key][word] for key in self.bib][pos]
+        self.token_list=self.start
         return 
     
     def one_long_one(self):
-        from functools import reduce
         self.token_list=reduce(lambda a,b:a+b,[self.bib[key][1] for key in self.bib])
         return
     
@@ -33,6 +40,12 @@ class augmentor(spell_mistake,aug_loader):
     def return_vectors(self):
         [stdout.write(str(self.find_it(i))+',') for i in self.token_list]
         return 
+    
+    def __str__(self):
+        return 'There are '+str(len(self.bib))+' Texts in Corpus\n'+ \
+                'Current word count is '+ str(len(self.token_list))+ \
+                '\nNumber of applied transformations is : '+ \
+                str(sum([True for i,x in enumerate(self.token_list) if self.start[i]!=x]))
 
 if __name__ == "__main__":
     liste=[os.path.abspath(''),
